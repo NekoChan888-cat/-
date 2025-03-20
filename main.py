d783b135735b0b333dd91bf04459f1e375c8d669
Лерусик <v.ginina04@gmail.com>
2025-03-21 01:04:43 +0300

    Update1 main.py

--- main.py
+++ main.py
@@ -1,21 +1,40 @@
-print("Введите два числа и операцию: ")
+def calculator():
+    print("Калькулятор")
+    print("Введите 'exit' для выхода.")
+    
+    while True:
+        num1 = input("Первое число: ")
+        if num1.lower() == 'exit':
+            break
+            
+        operation = input("Введите операцию (+, -, *, /): ")
+        if operation.lower() == 'exit':
+            break
+            
+        num2 = input("Второе число: ")
+        if num2.lower() == 'exit':
+            break
+        
+        try:
+            num1 = float(num1)
+            num2 = float(num2)
 
-num1 = float(input("Первое число: "))
-operation = input("Введите операцию (+, -, *, /): ")
-num2 = float(input("Второе число: "))
+            if operation == '+':
+                result = num1 + num2
+            elif operation == '-':
+                result = num1 - num2
+            elif operation == '*':
+                result = num1 * num2
+            elif operation == '/':
+                if num2 != 0:
+                    result = num1 / num2
+                else:
+                    result = "Ошибка: деление на ноль!"
+            else:
+                result = "Неизвестная операция!"
+                
+            print("Результат:", result)
+        except ValueError:
+            print("Ошибка: введите правильные числа.")
 
-if operation == '+':
-    result = num1 + num2
-elif operation == '-':
-    result = num1 - num2
-elif operation == '*':
-    result = num1 * num2
-elif operation == '/':
-    if num2 != 0:
-        result = num1 / num2
-    else:
-        result = "Ошибка: деление на ноль!"
-else:
-    result = "Неизвестная операция!"
-
-print("Результат:", result)
+calculator()
